92a345032ea521536055ac26a2fb9c18d9c60d1e
MhawnSendes <jtheil22@student.aau.dk>
2024-10-18 11:44:38 +0200

    added mm 14 to hackerscript 2

--- Control/mm11to15/HackerScript2.m
+++ Control/mm11to15/HackerScript2.m
@@ -1,9 +1,12 @@
+s = tf('s');
+
 %For styrbar form mm12
 %Find chanonical controlable form
 A = [7 -9;
      6 -8];
 B = [4;
      3];
+C = [1 1];
 Cc = [B, A*B];
 s2 = [0 1]*inv(Cc);
 s1 = s2*A;
@@ -19,7 +22,6 @@ Ac+[1;0]*Fx;
 
 %For obsaverbar form mm13
 %initialisering
-C = [1 1];
 Lo =[-8;
      -22];
 O = [C;
@@ -38,4 +40,21 @@ L = To*Lo;
 eig(Ao+Lo*Co);
 eig(A+L*C);
 eig([A B*F; ...
-     -L*C A+L*C+B*F])
+     -L*C A+L*C+B*F]);
+
+%mm14 her
+Te = [1 -1;
+     0 1];
+Ae = inv(Te)*A*Te;
+Be = inv(Te)*B;
+Ce = C*Te;
+Le = -1/3;
+Fe = F*Te;
+Ar = Ae(2,2) + Le*Ae(1,2) + (Be(2)+Le*Be(1))*Fe(2);
+Br = Ae(2,1) + Le*Ae(1,1) + (Be(2)+Le*Be(1))*Fe(1) - Ar*Le;
+Cr = Fe(2);
+Dr = Fe(1) - Fe(2)*Le;
+K = Cr*inv((s*eye(1) - Ar))*Br + Dr;
+H = ss(A,B,C,zeros(1,1));
+sysCL = feedback(K,H,1);
+zpk(sysCL)
