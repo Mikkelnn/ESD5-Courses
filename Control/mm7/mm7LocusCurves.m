ee12b8eae0ca1ad30e7c51163a41d004dddbba7d
MhawnSendes <jtheil22@student.aau.dk>
2025-01-09 13:34:41 +0100

    added some more scripts for more mm's

--- Control/mm7/mm7LocusCurves.m
+++ Control/mm7/mm7LocusCurves.m
@@ -1,13 +1,19 @@
 s=tf('s');
 SysA = (s+4)/(s*(s+2));
+figure;
 rlocus(SysA)
 SysB = (s+4)/((s+2+i*1)*(s+2-i*1));
+figure;
 rlocus(SysB)
 SysC = (s+4)/s^2;
+figure;
 rlocus(SysC)
 SysD = ((s+6)*(s+4))/(s*(s+2));
+figure;
 rlocus(SysD)
 SysE = 1/((s+2+i*1)*(s+2-i*1)*(s-2));
+figure;
 rlocus(SysE)
 SysF = (s+2)/((s+4+i*2)*(s+4-i*2)*(s-2)*(s+i*1)*(s-i*1));
+figure;
 rlocus(SysF)
