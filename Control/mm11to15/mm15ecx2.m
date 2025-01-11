5037852d5fce500963eeaf7dba10ce718e096fa7
Mikkel <mikkel@norre.cc>
2025-01-11 13:54:45 +0100

    added stuff

--- Control/mm11to15/mm15ecx2.m
+++ Control/mm11to15/mm15ecx2.m
@@ -13,21 +13,29 @@ Cc = [B, A*B];
 
 rho_values = [0.1, 0.5, 1, 10, 100, 1000];
 Fopts = cell(1, length(rho_values));
-eig_results = zeros(2, length(rho_values)); % Preallocate for eigenvalues
-zero()
+eig_results = zeros(2, length(rho_values)); % Preallocate for eigenvalues (poles)
+zero_results = zeros(2, length(rho_values)); % Preallocate for zeros
 
 for i = 1:length(rho_values)
     rho = rho_values(i);
     Fopts{i} = -lqr(A, B, rho * C' * C, 1);
     eig_results(:, i) = eig(A + B * Fopts{i});
+    zero_results(:, i) = zero(A + B * Fopts{i});
 end
 
 % Display Fopts and eig_results
+disp('rho values:');
+disp(rho);
+
+
 disp('Fopts:');
 disp(Fopts);
 
-disp('Eigenvalues:');
+disp('Eigenvalues (poles):');
 disp(eig_results);
+
+disp('(zeros):');
+disp(zero_results);
 
 
 
